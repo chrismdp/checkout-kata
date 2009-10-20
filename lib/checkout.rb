117a93825aaa23491515503b559d420ccef327d4
Chris Parsons <chris@chris3.local>
2009-10-20 22:35:38 +0100

    Attempt 3: 18m 27s.

--- lib/checkout.rb
+++ lib/checkout.rb
@@ -1,1 +1,27 @@
-# code for checkout goes here
+class Checkout
+  attr :total
+
+  def initialize
+    @total = 0
+    @count = {}
+  end
+  
+  def scan(item)
+    @total += PRICES[item]
+    DISCOUNTS.each do |code, discount|
+      if (code == item)
+        @count[code] ||= 0
+        @count[code] += 1
+        if (@count[code] == discount[:quantity])
+          @total -= discount[:amount]
+          @count[code] = 0
+        end
+      end
+    end
+  end
+  
+private
+  PRICES = {"A" => 50, "B" => 30, "C" => 20, "D" => 15}
+  DISCOUNTS = {'A' => {:quantity => 3, :amount => 20},
+                      'B' => {:quantity => 2, :amount => 15}}
+end
--- spec/checkout_spec.rb
+++ spec/checkout_spec.rb
@@ -1,4 +1,79 @@
 require File.dirname(__FILE__) + "/spec_helper"
 
 describe "Checkout" do
+  context "the total" do
+    def price(string)
+      checkout = Checkout.new
+      string.split(//).each do |item|
+        checkout.scan(item)
+      end
+      checkout.total
+    end
+    
+    it "is 0 when nothing has been added" do
+      price("").should == 0
+    end
+    
+    it "is 50 when A is added" do
+      price("A").should == 50
+    end
+    
+    it "is 80 when AB is added" do
+      price("AB").should == 80
+    end
+    
+    it "is 115 when CDBA is added" do
+      price("CDBA").should == 115
+    end
+
+    it "is 100 when AA is added" do
+      price("AA").should == 100
+    end
+    
+    it "is 130 when AAA is added" do
+      price("AAA").should == 130
+    end
+    
+    it "is 180 when AAAA is added" do
+      price("AAAA").should == 180
+    end
+    
+    it "is 230 when AAAAA is added" do
+      price("AAAAA").should == 230
+    end
+    
+    it "is 260 when AAAAAA is added" do
+      price("AAAAAA").should == 260
+    end
+    
+    it "is 160 when AAAB is added" do
+      price("AAAB").should == 160
+    end
+    
+    it "is 175 when AAABB is added" do
+      price("AAABB").should == 175
+    end
+    
+    it "is 190 when AAABBD is added" do
+      price("AAABBD").should == 190
+    end
+    
+    it "is 190 when DABADA is added" do
+      price("DABABA").should == 190
+    end
+    
+    it "handles incremental scan amounts" do
+      checkout = Checkout.new
+      checkout.scan('A')
+      checkout.total.should == 50
+      checkout.scan('B')
+      checkout.total.should == 80
+      checkout.scan('A')
+      checkout.total.should == 130
+      checkout.scan('A')
+      checkout.total.should == 160
+      checkout.scan('B')
+      checkout.total.should == 175
+    end
+  end
 end
