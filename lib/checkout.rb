02bb114819f6d82e8b925d820f43577ffd581b1b
Chris Parsons <chris@edendevelopment.co.uk>
2009-10-27 23:46:01 +0000

    16m 49s. Faster touch typing now. Late night so slower. Factored apply_discount a little too late.

--- lib/checkout.rb
+++ lib/checkout.rb
@@ -1,1 +1,28 @@
-# code for checkout goes here
+class Checkout
+  attr :total
+
+  def initialize
+    @total = 0
+    @count = {}
+  end
+
+  def scan(item)
+    @total += PRICE[item]
+    apply_discount(item)
+  end
+private
+  def apply_discount(item)
+    @count[item] ||= 0
+    @count[item] += 1
+    if (details = DISCOUNT[item])
+      if (@count[item] == details[:quantity])
+        @total -= details[:discount] 
+        @count[item] = 0
+      end
+    end
+  end
+
+  PRICE = { 'A' => 50, 'B' => 30, 'C' => 20, 'D' => 15 }
+  DISCOUNT = { 'A' => { :quantity => 3, :discount => 20 }, 'B' => { :quantity => 2, :discount => 15 } }
+end
+
