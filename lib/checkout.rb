db73a61bc726cf84a561fa5266bbb5545a0811b2
Chris Parsons <chris@edendevelopment.co.uk>
2009-10-27 15:25:23 +0000

    Attempt 7. Second using vi. Fairly happy with solution. Bit clunky towards the end. 22m 51s

--- lib/checkout.rb
+++ lib/checkout.rb
@@ -1,1 +1,24 @@
-# code for checkout goes here
+class Checkout
+  attr :total
+
+  def initialize
+    @total = 0
+    @count = {}
+  end
+
+  def scan(item)
+    @total += PRICE[item]
+    @count[item] ||= 0
+    @count[item] += 1
+    if (code = DISCOUNT[item])
+      if (@count[item] == DISCOUNT[item][:quantity])
+        @count[item] = 0
+        @total -= DISCOUNT[item][:discount]
+      end
+    end
+  end
+private
+  PRICE = { 'A' => 50, 'B' => 30, 'C' => 20, 'D' => 15 }
+  DISCOUNT = { 'A' => { :quantity => 3, :discount => 20 },
+               'B' => { :quantity => 2, :discount => 15 } }
+end
