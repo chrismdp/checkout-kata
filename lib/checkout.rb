c02d28d016db1ca6957eb72e3b08a5af58501fce
Chris Parsons <chris@edendevelopment.co.uk>
2009-11-05 17:28:04 +0000

    Attempt 13 14m25s Lots of feedback from Steve, James, Enrique
    - specs should be 'scanned' instead of 'added'
    - Hash.new(0)
    - Modulo for count quantity checks

--- lib/checkout.rb
+++ lib/checkout.rb
@@ -1,1 +1,25 @@
-# code for checkout goes here
+class Checkout
+  attr :total
+
+  def initialize
+    @total = 0
+    @count = Hash.new(0)
+  end
+
+  def scan(item)
+    @total += PRICE[item]
+    apply_discount(item)
+  end
+private
+  def apply_discount(item)
+    @count[item] += 1
+    if (details = DISCOUNT[item])
+      if @count[item] % details[:quantity] == 0
+        @total -= details[:discount]
+      end
+    end
+  end
+
+  PRICE = { 'A' => 50, 'B' => 30, 'C' => 20, 'D' => 15 }
+  DISCOUNT = { 'A' => {:quantity => 3, :discount => 20 }, 'B' => { :quantity => 2, :discount => 15 } }
+end
--- spec/checkout_spec.rb
+++ spec/checkout_spec.rb
@@ -1,1 +1,69 @@
 require File.dirname(__FILE__) + "/spec_helper"
+
+
+describe "Checkout" do
+  context "total" do
+    def price(string)
+      checkout = Checkout.new
+      string.split(//).each do |item|
+        checkout.scan(item)
+      end
+      checkout.total
+    end
+
+    it "is 0 if nothing has been scanned" do
+      price("").should == 0
+    end
+    it "is 50 if A is scanned" do
+      price('A').should == 50
+    end
+
+    it "is 80 if AB is scanned" do
+      price('AB').should == 80
+    end
+
+    it "is 115 if CDBA is scanned" do
+      price('CDBA').should == 115
+    end
+    it "is 100 if AA is scanned" do
+      price('AA').should == 100
+    end
+    it "is 130 if AAA is scanned" do
+      price('AAA').should == 130
+    end
+    it "is 180 if AAAA is scanned" do
+      price('AAAA').should == 180
+    end
+    it "is 230 if AAAAA is scanned" do
+      price('AAAAA').should == 230
+    end
+    it "is 260 if AAAAAA is scanned" do
+      price('AAAAAA').should == 260
+    end
+    it "is 160 if AAAB is scanned" do
+      price('AAAB').should == 160
+    end
+    it "is 175 if AAABB is scanned" do
+      price('AAABB').should == 175
+    end
+    it "is 190 if AAABBD is scanned" do
+      price('AAABBD').should == 190
+    end
+    it "is 190 if DABABA is scanned" do
+      price('DABABA').should == 190
+    end
+    it "handles incremental scanning" do
+      checkout = Checkout.new
+      checkout.scan('A')
+      checkout.total.should == 50
+      checkout.scan('B')
+      checkout.total.should == 80
+      checkout.scan('A')
+      checkout.total.should == 130
+      checkout.scan('A')
+      checkout.total.should == 160
+      checkout.scan('B')
+      checkout.total.should == 175
+    end
+  end
+end
