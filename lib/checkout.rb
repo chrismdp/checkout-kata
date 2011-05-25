3d0ca6ef727773026a42741f640c6f0817f90a63
Chris Parsons <chris@thinkcodelearn.com>
2011-05-25 20:54:53 +0100

    Iteration 1

--- lib/checkout.rb
+++ lib/checkout.rb
@@ -1,4 +1,17 @@
 class Checkout
-  def add(item)
+  def initialize
+    @total = 0
   end
+
+  def add(item)
+    @total += price(item)
+  end
+
+  private
+
+  def price(item)
+    PRICE[item] || 0
+  end
+
+  PRICE = {"Apples" => 100, "Cherries" => 75, "Bananas" => 150}
 end
--- spec/checkout_spec.rb
+++ spec/checkout_spec.rb
@@ -1,4 +1,28 @@
 require File.dirname(__FILE__) + "/spec_helper"
 
 describe Checkout do
+  let(:checkout) { Checkout.new }
+  context "add" do
+    it "accepts Apples" do
+      checkout.add("Apples").should == 100
+      checkout.add("Apples").should == 200
+    end
+    it "also accepts Cherries" do
+      checkout.add("Apples").should == 100
+      checkout.add("Cherries").should == 175
+      checkout.add("Cherries").should == 250
+    end
+
+    it "accepts Bananas" do
+      checkout.add("Bananas").should == 150
+    end
+
+    it "ignores failed scans" do
+      checkout.add("FOO").should == 0
+      checkout.add("Bananas").should == 150
+      checkout.add("FOO").should == 150
+    end
+      
+  end
+
 end
